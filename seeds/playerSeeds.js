b35c78b14f7286c3a1c41eca9e7f01ef8b8076fb
waltscode <walterswork91@outlook.com>
2024-04-02 17:51:09 -0400

    added the toronto maple leafs roster

--- seeds/playerSeeds.js
+++ seeds/playerSeeds.js
@@ -1,20 +1,238 @@
 const {Player} = require('../models');
 
 const playerData = [
-    { player_name: 'LeBron James', player_number: 23, player_position: 'SF', player_age: 36, player_height: '6-9', player_weight: 250, player_college: 'St. Vincent-St. Mary HS (OH)', player_team: 1, player_salary: 39219565 },
-
-    { player_name: 'Anthony Davis', player_number: 3, player_position: 'PF', player_age: 28, player_height: '6-10', player_weight: 253, player_college: 'Kentucky', player_team: 1, player_salary: 32742000 },
-
-    { player_name: 'Stephen Curry', player_number: 30, player_position: 'PG', player_age: 33, player_height: '6-3', player_weight: 185, player_college: 'Davidson', player_team: 2, player_salary: 43006362 },
-
-    { player_name: 'Klay Thompson', player_number: 11, player_position: 'SG', player_age: 31, player_height: '6-6', player_weight: 215, player_college: 'Washington State', player_team: 2, player_salary: 35573500 },
-
-    { player_name: 'Kawhi Leonard', player_number: 2, player_position: 'SF', player_age: 30, player_height: '6-7', player_weight: 225, player_college: 'San Diego State', player_team: 3, player_salary: 34449964 },
-
-    { player_name: 'Paul George', player_number: 13, player_position: 'SF', player_age: 31, player_height: '6-8', player_weight: 220, player_college: 'Fresno State', player_team: 3, player_salary: 35450412 },
-
-    { player_name: 'Devin Booker', player_number: 1, player_position: 'SG', player_age: 24, player_height: '6-5', player_weight: 206, player_college: 'Kentucky', player_team: 4, player_salary: 29475354 },
-
+    //Toronto Maple Leafs
+    {
+        "player_name": "Auston Matthews",
+        "player_number": 34,
+        "player_position": "C",
+        "player_age": 24,
+        "player_height": "6-3",
+        "player_weight": 223,
+        "player_college": "N/A (Drafted from ZSC Lions)",
+        "player_team": 28,
+        "player_salary": 11666667
+    },
+    {
+        "player_name": "Mitch Marner",
+        "player_number": 16,
+        "player_position": "RW",
+        "player_age": 24,
+        "player_height": "6-0",
+        "player_weight": 175,
+        "player_college": "N/A (Drafted from London Knights)",
+        "player_team": 28,
+        "player_salary": 10600000
+    },
+    {
+        "player_name": "John Tavares",
+        "player_number": 91,
+        "player_position": "C",
+        "player_age": 31,
+        "player_height": "6-1",
+        "player_weight": 205,
+        "player_college": "London Knights",
+        "player_team": 28,
+        "player_salary": 11000000
+    },
+    {
+        "player_name": "William Nylander",
+        "player_number": 88,
+        "player_position": "RW",
+        "player_age": 25,
+        "player_height": "6-0",
+        "player_weight": 196,
+        "player_college": "Rögle BK (SHL)",
+        "player_team": 28,
+        "player_salary": 7000000
+    },
+    {
+        "player_name": "Morgan Rielly",
+        "player_number": 44,
+        "player_position": "D",
+        "player_age": 27,
+        "player_height": "6-1",
+        "player_weight": 217,
+        "player_college": "Moose Jaw Warriors",
+        "player_team": 28,
+        "player_salary": 5000000
+    },
+    {
+        "player_name": "Jake Muzzin",
+        "player_number": 8,
+        "player_position": "D",
+        "player_age": 33,
+        "player_height": "6-3",
+        "player_weight": 213,
+        "player_college": "Sault Ste. Marie Greyhounds",
+        "player_team": 28,
+        "player_salary": 5250000
+    },
+    {
+        "player_name": "Jack Campbell",
+        "player_number": 36,
+        "player_position": "G",
+        "player_age": 30,
+        "player_height": "6-2",
+        "player_weight": 197,
+        "player_college": "Port Huron Fighting Falcons",
+        "player_team": 28,
+        "player_salary": 1675000
+    },
+    {
+        "player_name": "Michael Bunting",
+        "player_number": 58,
+        "player_position": "LW",
+        "player_age": 26,
+        "player_height": "5-11",
+        "player_weight": 203,
+        "player_college": "Soo Greyhounds",
+        "player_team": 28,
+        "player_salary": 950000
+    },
+    {
+        "player_name": "Wayne Simmonds",
+        "player_number": 24,
+        "player_position": "RW",
+        "player_age": 33,
+        "player_height": "6-2",
+        "player_weight": 185,
+        "player_college": "Soo Greyhounds",
+        "player_team": 28,
+        "player_salary": 900000
+    },
+    {
+        "player_name": "T.J. Brodie",
+        "player_number": 78,
+        "player_position": "D",
+        "player_age": 31,
+        "player_height": "6-1",
+        "player_weight": 182,
+        "player_college": "Saginaw Spirit",
+        "player_team": 28,
+        "player_salary": 5000000
+    },
+    {
+        "player_name": "David Kampf",
+        "player_number": 64,
+        "player_position": "C",
+        "player_age": 27,
+        "player_height": "6-2",
+        "player_weight": 194,
+        "player_college": "Pirati Chomutov (Czech)",
+        "player_team": 28,
+        "player_salary": 1500000
+    },
+    {
+        "player_name": "Pierre Engvall",
+        "player_number": 47,
+        "player_position": "LW",
+        "player_age": 25,
+        "player_height": "6-5",
+        "player_weight": 214,
+        "player_college": "Frolunda HC (SHL)",
+        "player_team": 28,
+        "player_salary": 1750000
+    },
+    {
+        "player_name": "Ilya Mikheyev",
+        "player_number": 65,
+        "player_position": "LW",
+        "player_age": 26,
+        "player_height": "6-3",
+        "player_weight": 194,
+        "player_college": "Avangard Omsk (KHL)",
+        "player_team": 28,
+        "player_salary": 1500000
+    },
+    {
+        "player_name": "Travis Dermott",
+        "player_number": 23,
+        "player_position": "D",
+        "player_age": 25,
+        "player_height": "6-0",
+        "player_weight": 208,
+        "player_college": "Erie Otters",
+        "player_team": 28,
+        "player_salary": 1500000
+    },
+    {
+        "player_name": "Justin Holl",
+        "player_number": 3,
+        "player_position": "D",
+        "player_age": 29,
+        "player_height": "6-4",
+        "player_weight": 210,
+        "player_college": "Minnesota Golden Gophers",
+        "player_team": 28,
+        "player_salary": 2000000
+    },
+    {
+        "player_name": "Jason Spezza",
+        "player_number": 19,
+        "player_position": "C",
+        "player_age": 38,
+        "player_height": "6-3",
+        "player_weight": 214,
+        "player_college": "N/A (Drafted from Brampton Battalion)",
+        "player_team": 28,
+        "player_salary": 750000
+    },
+    {
+        "player_name": "Timothy Liljegren",
+        "player_number": 37,
+        "player_position": "D",
+        "player_age": 22,
+        "player_height": "6-0",
+        "player_weight": 192,
+        "player_college": "Toronto Marlies",
+        "player_team": 28,
+        "player_salary": 863333
+    },
+    {
+        "player_name": "Rasmus Sandin",
+        "player_number": 38,
+        "player_position": "D",
+        "player_age": 21,
+        "player_height": "5-11",
+        "player_weight": 183,
+        "player_college": "Toronto Marlies",
+        "player_team": 28,
+        "player_salary": 894166
+    },
+    {
+        "player_name": "Joseph Woll",
+        "player_number": 60,
+        "player_position": "G",
+        "player_age": 23,
+        "player_height": "6-4",
+        "player_weight": 203,
+        "player_college": "Boston College",
+        "player_team": 28,
+        "player_salary": 850000
+    },
+    {
+        "player_name": "Ondrej Kase",
+        "player_number": 25,
+        "player_position": "RW",
+        "player_age": 26,
+        "player_height": "6-0",
+        "player_weight": 186,
+        "player_college": "Pardubice HC (Czech)",
+        "player_team": 28,
+        "player_salary": 2000000
+    },
+    {
+        "player_name": "Nick Ritchie",
+        "player_number": 10,
+        "player_position": "LW",
+        "player_age": 26,
+        "player_height": "6-2",
+        "player_weight": 232,
+        "player_college": "Peterborough Petes",
+        "player_team": 28,
+        "player_salary": 2250000
+    }
 ];
 
 const seedPlayers = () => Player.bulkCreate(playerData);
